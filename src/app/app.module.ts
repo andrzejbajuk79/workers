179500b244b7e5508d049984ac199c1c2f1c5257
ptasiek3003 <ptasiek3003@gmail.com>
2018-11-27 20:54:12 +0100

    comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './workers/employess/list-employees.component';
+import { CreateEmployeeComponent } from './workers/create-employee/create-employee.component';
 
+
+const appRoutes: Routes = [
+{ path: 'list', component: ListEmployeesComponent },
+{ path: 'create', component: CreateEmployeeComponent },
+{ path: '', redirectTo: '/list', pathMatch: 'full' },
+{path: '**',  redirectTo: '/list', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/workers/create-employee/create-employee.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../../models/department.model';
+import { BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+datePickerConfig: Partial<BsDatepickerConfig>;
+fullName: string;
+email: string;
+gender = 'male';
+isActive = true;
+contactPreference = 'mail';
+departments: Department[] = [
+  {id: 1, name: 'Help Desk'},
+  {id: 2, name: 'IT'},
+  {id: 3, name: 'HR'},
+];
+
+  constructor() {
+    this.datePickerConfig = Object.assign({},
+      {
+        containerClass: 'theme-dark-blue',
+        showWeekNumbers: false,
+        minDate: new Date(1978, 0, 1),
+        maxDate: new Date(1979, 0, 1),
+        dateInputFormat: 'DD/MM/YYYY'
+      });
+   }
+
+  ngOnInit() {
+  }
+  onSubmit(empForm: NgForm ): void {
+console.log(empForm);
+console.log(empForm.value);
+  }
+
+}
